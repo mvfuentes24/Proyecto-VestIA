b13ff75a51f56e82e35568d0454e401e947ec669
Maria <mvfuentes.24@est.ucab.edu.ve>
2026-01-02 22:49:50 -0400

    para renderizar productos y catalogo

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
+// para el dummyJSON
+export const DUMMYJSON_BASE = 'https://dummyjson.com';
+
+
+export const GEMINI_MODEL = 'gemini-2.5-flash';
+
+
+export const GEMINI_API_KEY = 'TU_API_KEY_AQUI';
+
 

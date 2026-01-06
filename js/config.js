7c2779b4e35bc1d1180ca492469980eec9286c27
luisenr1412 <lechavero.24@est.ucab.edu.ve>
2026-01-06 17:01:29 -0400

    Remove sensitive API key from config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const DUMMYJSON_BASE = 'https://dummyjson.com';
 export const GEMINI_MODEL = 'gemini-2.5-flash';
-export const GEMINI_API_KEY = 'AIzaSyDJHAx-nvmF8FPFm-060NQaxT3yE19E9Gg';
+export const GEMINI_API_KEY = '';
 
 
 

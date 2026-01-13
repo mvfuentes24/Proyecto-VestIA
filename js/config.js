8a6e599c79418c78b687f8ec176f3117aeee80e2
Luis <lechavero.24@est.ucab.edu.ve>
2026-01-12 23:42:26 -0400

    solucion key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const DUMMYJSON_BASE = 'https://dummyjson.com';
 export const GEMINI_MODEL = 'gemini-2.5-flash';
-export const GEMINI_API_KEY = 'AIzaSyCuGHtm3kIuQ3zcYT8C1ecSDorFETKd1BQ';
+export const GEMINI_API_KEY = 'AIzaSyAzZ-qGjb3I-Bd1dKFO_ABGccyZl5tyYGE';
 export const CART_STORAGE_KEY = 'vestia_cart';
 export const KEY_PREFERENCIAS = "vestia_user_prefs";
 export const KEY_CHAT_HISTORY = "vestia_chat_history";

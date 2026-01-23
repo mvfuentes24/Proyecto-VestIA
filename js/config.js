2b799b3dc992035d6149fbf358291673bed43eca
Maria <mvfuentes.24@est.ucab.edu.ve>
2026-01-22 22:07:14 -0400

    arreglo key

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // para el dummyJSON
 export const DUMMYJSON_BASE = 'https://dummyjson.com';
 export const GEMINI_MODEL = 'gemini-2.5-flash';
-export const GEMINI_API_KEY = 'AIzaSyC8u9IZQArVcb2G8b9ap5dez_6cywxjJ6I';
+export const GEMINI_API_KEY = 'AIzaSyBEhybgynxNMquqdk3oQYOamfz5xBs_K6Y';
 export const CART_STORAGE_KEY = 'vestia_cart';
 export const KEY_PREFERENCIAS = "vestia_user_prefs";
 export const KEY_CHAT_HISTORY = "vestia_chat_history";
